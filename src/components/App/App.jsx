02a2ed9965ddbda1afe7f523f7ba703987e25769
AndersonConne <andyconne56@gmail.com>
2025-01-03 17:13:56 -0600

    changes after code review 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -63,13 +63,14 @@ function App() {
         title="New Garment"
         activeModal={activeModal}
         onClose={closeActiveModal}
+        isOpen={"add-garment"}
       >
         <label htmlFor="name" className="modal__label">
           Name{" "}
           <input
             type="text"
             className="modal__input"
-            id="Name"
+            id="name"
             placeholder="Name"
           />
         </label>
@@ -88,19 +89,37 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+              name="radio"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,15 +1,18 @@
 import "../ModalWithForm/ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  onClose,
+  isOpen,
+}) {
   return (
-    <div className={`modal ${activeModal === "add-garment" && "modal_open"}`}>
+    <div className={`modal ${activeModal === isOpen && "modal_open"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button
-          type="button"
-          onClick={onClose}
-          className="modal__close"
-        ></button>
+        <button type="button" onClick={onClose} className="modal__close" />
         <form className="modal__form">
           {children}
           <button className="modal__submit" type="submit">
